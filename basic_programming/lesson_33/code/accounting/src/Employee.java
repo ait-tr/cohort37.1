4a8b22f75be2e0bc991707550bb6dfeb5fa859b5
Evgenygroup <mailfrom61@gmail.com>
2024-01-08 13:23:42 +0100

    adds lesson 33

---
+++ basic_programming/lesson_33/code/accounting/src/Employee.java
@@ -0,0 +1,44 @@
+public abstract class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double hour;
+
+
+    public Employee(int id, String firstName, String lastName, double hour) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hour = hour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getHour() {
+        return hour;
+    }
+
+
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", hour=" + hour +
+                '}';
+    }
+}
